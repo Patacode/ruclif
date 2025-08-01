a4477ff8936acffe6a60890ce8c701f3a7a75b5f
Patacode <pata.codegineer@gmail.com>
2025-08-01 02:34:51 +0200

    chore(ruclif-parser-clap):  rectify byte size estimation with char taking less

--- ruclif-parser-clap/src/lib.rs
+++ ruclif-parser-clap/src/lib.rs
@@ -3,7 +3,7 @@ pub mod string;
 #[derive(Default, Debug)]
 struct ClapArgData {
     name: Option<&'static str>, // 16 bytes
-    short: Option<char>, // 16 bytes
+    short: Option<char>, // 4 bytes
     long: Option<&'static str>, // 16 bytes
     description: Option<&'static str>, // 16 bytes
-} // 64 bytes
+} // 56 bytes (4 more due to padding)
--- ruclif-parser-clap/src/string.rs
+++ ruclif-parser-clap/src/string.rs
@@ -4,8 +4,7 @@ mod impls;
 
 #[derive(Default, Debug)]
 pub struct StringClapArgData {
-    common: ClapArgData
-, // 64 bytes
+    common: ClapArgData, // 64 bytes
     default_value: Option<&'static str>, // 16 bytes
     value_parser: Option<fn(&str) -> Result<&str, &str>>, // 8 bytes
 } // 88 bytes
@@ -19,7 +18,7 @@ pub struct StringClapArg {
 #[derive(Default, Debug)]
 pub struct StringClapArgBuilder {
     name: Option<&'static str>, // 16 bytes
-    short: Option<&'static str>, // 16 bytes
+    short: Option<char>, // 4 bytes
     long: Option<&'static str>, // 16 bytes
     description: Option<&'static str>, // 16 bytes
     default_value: Option<&'static str>, // 16 bytes
