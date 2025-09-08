637a4f6da9d4a075806979dffb60bd9a2356141f
Patacode <pata.codegineer@gmail.com>
2025-09-08 22:57:14 +0200

    test: refine flag arg director

--- ruclif-parser-clap/src/flag/tests/director.rs
+++ ruclif-parser-clap/src/flag/tests/director.rs
@@ -1,16 +1,16 @@
 use crate::{
-    flag::{FlagArgAction, FlagClapArgData},
-    ClapArgData,
+    flag::{FlagAction, FlagClapArgData},
+    ClapNamedArgData,
 };
 
 pub mod defaults {
-    use crate::flag::FlagArgAction;
+    use super::*;
 
     pub const NAME: &str = "AUTHOR";
     pub const SHORT: char = 'a';
     pub const LONG: &str = "author";
     pub const DESCRIPTION: &str = "Print author";
-    pub const ACTION: FlagArgAction = FlagArgAction::SetFalse;
+    pub const ACTION: FlagAction = FlagAction::SetFalse;
 }
 
 pub struct TestData {
@@ -34,7 +34,7 @@ impl TestData {
         self.arg_data.common.description
     }
 
-    pub fn action(&self) -> &FlagArgAction {
+    pub fn action(&self) -> &FlagAction {
         &self.arg_data.action
     }
 }
@@ -43,7 +43,7 @@ impl TestData {
     pub fn with_all_fields_set() -> Self {
         Self {
             arg_data: FlagClapArgData {
-                common: ClapArgData {
+                common: ClapNamedArgData {
                     name: defaults::NAME,
                     short: defaults::SHORT,
                     long: defaults::LONG,
