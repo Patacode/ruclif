ac6a5f911b3c29892cd87b330c1ddd2008e83a84
Patacode <pata.codegineer@gmail.com>
2025-09-25 20:36:59 +0200

    style(ruclif-parser-clap): apply clearer formatting to flag module

--- ruclif-parser-clap/src/flag.rs
+++ ruclif-parser-clap/src/flag.rs
@@ -22,22 +22,24 @@ pub struct FlagClapArg {
     data: FlagClapArgData,
 }
 
+#[rustfmt::skip]
 #[derive(Default, Debug, PartialEq, Eq, Clone)]
 pub struct FlagClapArgBuilder {
-    name: Option<&'static str>,
-    short: Option<char>,
-    long: Option<&'static str>,
+    name:        Option<&'static str>,
+    short:       Option<char>,
+    long:        Option<&'static str>,
     description: Option<&'static str>,
-    action: Option<FlagAction>,
-    state: u8,
+    action:      Option<FlagAction>,
+    state:       u8,
 }
 
+#[rustfmt::skip]
 mod builder_state {
-    pub const NAME: usize = 0b1;
-    pub const SHORT: usize = 0b10;
-    pub const LONG: usize = 0b100;
+    pub const NAME:        usize = 0b0001;
+    pub const SHORT:       usize = 0b0010;
+    pub const LONG:        usize = 0b0100;
     pub const DESCRIPTION: usize = 0b1000;
-    pub const EXPECTED: u8 = 0b1111;
+    pub const EXPECTED:    u8    = 0b1111;
 }
 
 mod error_message {
